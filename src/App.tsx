20cf1f4de2314dd9524b4ac9d6d71de6d1c3047f
Mabson Vinicius <mabson.vinicius@supermercadospaguemenos.com.br>
2022-08-10 18:00:15 -0300

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,28 @@
+import { gql } from "@apollo/client"
+import { useEffect } from "react"
+import { client } from "./lib/apollo"
+
+const GET_LESSONS_QUERY = gql`
+  query {
+    lessons {
+      id
+      title
+    }
+  }
+`
+
+function App() {
+  useEffect(() => {
+    client.query(
+      {
+        query: GET_LESSONS_QUERY
+      }
+    )
+  }, [])
+
+  return (
+    <h1 className="">Hello World</h1>
+  )
+}
+
+export default App
